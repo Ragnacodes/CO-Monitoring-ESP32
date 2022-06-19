ee7b7b59c809b15e17b898a644ed85abe658dcfc
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2022-06-20 03:10:50 +0430

    Connect the esp32 to the backend

--- co_monitoring.ino
+++ co_monitoring.ino
@@ -1,59 +1,65 @@
+// Wifi and internet libraries
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char* ssid = "Mike Wazowski";
-const char* password = "*******";
 
-String serverName = "https://webhook.site/2830263a-99eb-4a82-ac36-98a0fa0826ac";
+// Define pins
+#define ONBOARD_LED  2
+#define LED          13
 
-unsigned long lastTime = 0;
-unsigned long timerDelay = 5000;
+// ESP Serial
+const char* DEVICE_SERIAL = "78e76d25-6b1e-445e-b565-18e5a320d7ef";
+const char* DEVICE_ORG = "2a1f0c66-2c9f-4d1a-86b0-c4759efce4c1";
+
+// Wifi information
+bool isWifiConnected = false;
+const char* WIFI_SSID = "Mike Wazowski";
+const char* WIFI_PASSWORD = "********";
+
+// Server information
+const String BASE_URL = "http://188.121.110.10:8080/api/v1";
 
 void setup() {
-  Serial.begin(115200); 
+  // Setup serial
+  Serial.begin(9600);
+  Serial.println();
 
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
+  // Setup pin modes
+  pinMode(ONBOARD_LED,OUTPUT);
+  pinMode(LED, OUTPUT);
+
+  // Setup Wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(100);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  isWifiConnected = true;
 }
 
 void loop() {
-  if ((millis() - lastTime) > timerDelay) {
-    if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
+  updateState();
+  delay(10000);
+  
+  // HTTP Request
+  HTTPClient http;
+  String url = BASE_URL + "/organizations/" + DEVICE_ORG + "/devices/" + DEVICE_SERIAL + "/metrics/co_density";
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+  String httpRequestData = "{\"density\":" + String((float)random(0, 1000)/10.00) + "}";
+  Serial.println("HTTP URL is " + url);
+  Serial.println("HTTP Body is " + httpRequestData);
+  int httpResponseCode = http.PUT(httpRequestData);
+  Serial.print("The response code is: ");
+  Serial.println(httpResponseCode);
+  http.end();
+}
 
-//      String serverPath = serverName + "?temperature=24.37";
 
-      String serverPath = serverName;
-      
-      http.begin(serverPath.c_str());
-      
-      int httpResponseCode = http.GET();
-      
-      if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.println(payload);
-      }
-      else {
-        Serial.print("Error code: ");
-        Serial.println(httpResponseCode);
-      }
-      // Free resources
-      http.end();
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
-  }
+void updateState() {
+  digitalWrite(ONBOARD_LED, int(isWifiConnected));
 }
